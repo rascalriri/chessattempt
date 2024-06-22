5d2fdc87bbac7a65f6d11a15e88856de9204ff88
rascalriri <rascalriri@gmail.com>
2024-06-22 14:31:21 -0600

    Update chessattempt.cpp

--- chessattempt.cpp
+++ chessattempt.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 #include <string>
 
-using namespace std
+using namespace std;
 
-Class Square
-Squares[64]
+class Square
+{
+public:
+   enum status;
+{
+none, 
+white,
+black
+};
+  enum piecetype;  // ID would look like [index](status, piecetype) = [i](0,0) 
+{none, 
+pawn, 
+knight, 
+bishop, 
+queen, 
+king, 
+rook, 
+pikeman,  
+fanatic, 
+skeleton,
+hero
+};
 
-void board()
-{ for i in Squares, i++; // puts normal pieces on square they belong to //
+void board(){
+Square Squares[64];
+for (i = 0; i < 16; i++); // setting white and black//
+Squares[i].status = 1
   
+for (i = 48; i < 64; i++);
+Squares[i].status = 2
+
+for (i = 8; i < 16; i++);
+Squares[i].
+  // set square value of starting position 
+
