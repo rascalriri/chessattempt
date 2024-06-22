f29fa1a1757297d53e51e49692c23a5f38ecbb16
rascalriri <rascalriri@gmail.com>
2024-06-22 12:56:56 -0600

    Create chessattempt.cpp

---
+++ chessattempt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std
+
+Class Square
+Squares[64]
+
+void board()
+{ for i in Squares, i++; // puts normal pieces on square they belong to //
+  
